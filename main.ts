bcf41120b3d3b80d3b22b1dbfdfa9571ec1fdb0c
ScienceGuy318 <122555174+ScienceGuy318@users.noreply.github.com>
2023-01-24 13:05:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,45 @@
-let hand = 0
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-        hand = 0
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.ringTone(988)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("- G G G - - D - ", 120)
+})
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # . .
+            # # # # .
+            # # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
     }
 })
+let hand = 0
+basic.showLeds(`
+    . . . . .
+    # . # . #
+    # # # . #
+    # . # . #
+    . . . . .
+    `)
